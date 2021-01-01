680388dc1ac005b1c49da9c473296baaa5217f8b
mileslucey <43838442+mileslucey@users.noreply.github.com>
2021-01-01 02:14:19 -0800

    Update lyrics_scraper.py

--- lyrics_scraper.py
+++ lyrics_scraper.py
@@ -1,6 +1,7 @@
 # Dependencies and set up
 from bs4 import BeautifulSoup
 import requests
+import os
 
 def scrape(artist, song): # Function scrapes the AZ Lyrics website for song lyrics using the inputs defined by the user
     url = 'https://www.azlyrics.com/lyrics/' + artist + '/' + song + '.html'
@@ -10,8 +11,14 @@ def scrape(artist, song): # Function scrapes the AZ Lyrics website for song lyri
     division_list = article_body.find_all('div')
     file_name = artist + '_' + song + '_lyrics.txt'
     with open(file_name, 'w') as write_file:
-        for i in range(17,21,3):
-            write_file.write(division_list[i].text)
+        try:
+            for i in range(17,21,3):
+                write_file.write(division_list[i].text)
+        except Exception:
+            print('The site doesn\'t have lyrics for that song. Try again!')
+            write_file.close()
+            os.remove(file_name)
+            main()
 
 def main(): # Function prompts user input for artist and song title and calls the scrape function
     artist = input('Enter the artist\'s name: ').strip().lower()
